a876e58ccce7be36f9d3441488edd88213fadd43
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-02-22 12:19:33 +0700

    initial

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    this.appService.axiosPOST();
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,54 @@
 import { Injectable } from '@nestjs/common';
+const axios = require('axios').default;
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  axiosPOST(): void{
+    axios.post('/user', {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });;
+  }
+  axiosGET():void{
+    axios.get('/user', {
+      params: {
+        ID: 12345
+      }
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+  axiosPUT():void{
+    axios.put('/user', {
+      firstName: 'Fred',
+      lastName: 'Flintstone'
+    })
+  }
+  axiosPATCH():void{
+    axios.patch('/user', {
+      firstName: 'PATCHED',
+      lastName: 'PATCHED'
+    })
+  }
+  axiosDELETE():void{
+    axios.delete('/user', {
+      firstName: 'Fred',
+    })
   }
 }
